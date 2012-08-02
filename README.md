af7a0dc346fde48a1aa96a3548ae904d8d6ffec9
Sebastian Spieszko <sebastian@spieszko.pl>
2012-08-03 01:20:32 +0200

    Chef-solo install script

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ ubuntu-chef-solo
 ================
 
 Bootstrap Ubuntu Desktop.
+
+Run install.sh on fresh system as root to make sure,
+that system is upgraded and chef-solo is installed.
+
+Copypasted from
+===============
+
+* [Chef Solo tutorial: Managing a single server with Chef](http://www.opinionatedprogrammer.com/2011/06/chef-solo-tutorial-managing-a-single-server-with-chef/)
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# This runs as root
+
+chef_binary=/var/lib/gems/1.9.1/bin/chef-solo
+
+# Are we on a vanilla system?
+if ! test -f "$chef_binary"; then
+    export DEBIAN_FRONTEND=noninteractive
+    # Upgrade headlessly (this is only safe-ish on vanilla systems)
+    apt-get update &&
+    apt-get -o Dpkg::Options::="--force-confnew" \
+        --force-yes -fuy dist-upgrade &&
+    # Install Ruby and Chef
+    apt-get install -y ruby1.9.1 ruby1.9.1-dev make &&
+    sudo gem1.9.1 install --no-rdoc --no-ri chef --version 0.10.0
+fi
