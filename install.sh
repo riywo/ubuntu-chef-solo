b423186f51fad087c7b1cff5320b444dce13e571
riywo <riywo.jp@gmail.com>
2012-11-17 22:15:13 -0800

    misc

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sudo rm -rf ~/.ubuntu-chef-solo
-mkdir ~/.ubuntu-chef-solo
-cd ~/.ubuntu-chef-solo
+sudo rm -rf /tmp/ubuntu-chef-solo
+mkdir /tmp/ubuntu-chef-solo
+cd /tmp/ubuntu-chef-solo
 wget -nv -O - https://github.com/riywo/ubuntu-chef-solo/tarball/master | tar xz
 balldir=`ls`
 mv $balldir/* .
