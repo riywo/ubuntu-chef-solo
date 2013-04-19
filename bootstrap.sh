b63936286f53de5c0a48fb9343adf70e146c8c77
Ryosuke IWANAGA <riywo.jp@gmail.com>
2013-04-18 21:28:21 -0700

    rm dist-upgrade

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,9 +6,7 @@ if ! chef-solo -v > /dev/null 2>&1; then
     export DEBIAN_FRONTEND=noninteractive
     # Upgrade headlessly (this is only safe-ish on vanilla systems)
     apt-get update &&
-    apt-get -o Dpkg::Options::="--force-confnew" \
-        --force-yes -fuy dist-upgrade &&
     # Install Ruby and Chef
-    apt-get install -y ruby1.9.1 ruby1.9.1-dev make &&
-    sudo gem1.9.1 install --no-rdoc --no-ri chef
+    apt-get install -y ruby1.9.3 ruby1.9.3-dev build-essential &&
+    sudo gem install --no-rdoc --no-ri chef
 fi
